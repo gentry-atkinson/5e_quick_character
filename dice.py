37bf8227fd3b9646d8d2b60fce948651b827efee
gentry <gma23@txstate.edu>
2021-04-26 11:49:51 -0500

    added some more dice funcs

--- dice.py
+++ dice.py
@@ -1,6 +1,6 @@
 ###################################################################
 #Author: Gentry Atkinson                                          #
-#Date: 15 April, 2021                                             #
+#Date: 26 April, 2021                                             #
 #Description: this script will generate a level one character for #
 #  Dungeons and Dragons 5th Edition. All game is from the         #
 #  standard reference document.                                   #
@@ -15,6 +15,35 @@ def roll4drop1():
 def roll3():
     roll = [randint(1,6), randint(1,6), randint(1,6)]
     return sum(roll)
+
+def roll2add4():
+    roll = [randint(1,6), randint(1,6)]
+    return sum(roll)+4
+
+def roll(n):
+    return randint(1, n)
+
+def rollD4():
+    return roll(4)
+
+def rollD6():
+    return roll(6)
+
+def rollD8():
+    return roll(8)
+
+def rollD10():
+    return roll(10)
+
+def rollD12():
+    return roll(12)
+
+def rollD20():
+    return roll(20)
+
+def rollD100():
+    return roll(100)
+
 
 if __name__ == '__main__':
     print(roll4drop1())
